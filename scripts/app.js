a0b9510c9e6ac09b91676a0c74c60196056f4db1
grudinsky <g.rudzianskas@gmail.com>
2019-12-25 10:25:47 +0200

    mode toggle done

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,10 @@
 
 // SKILLS DATA
 const devIcons = [
-  {
-    name: 'Apple',
-    class: 'devicon-apple-original'
-  },
+  // {
+  //   name: 'Apple',
+  //   class: 'devicon-apple-original'
+  // },
   {
     name: 'Atom',
     class: 'devicon-atom-original'
@@ -169,7 +169,7 @@ function getDayTime(idName, content) {
   const t = new Date()
   const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   const dayHours = t.getHours()
-  const dayTime = dayHours < 12 ? 'morning' : (dayHours > 12 && dayHours < 17 ? 'afternoon' : 'evening')
+  const dayTime = dayHours < 12 ? 'morning' : (dayHours >= 12 && dayHours < 17 ? 'afternoon' : 'evening')
   f.innerHTML = content === 'weekDay' ? weekDays[t.getDay()] : (content === 'dayTime' ? dayTime : 'day')
   return f.innerHTML
 }
@@ -184,11 +184,11 @@ function displayProfileImage() {
 }
 
 function changeImages() {
-  const f = document.querySelector('.headshot')
+  const headshot = document.querySelector('.headshot')
   profileImages = profileImages.reverse()
   profileImages.forEach((image, i) => {
     setTimeout(() => {
-      f.style.backgroundImage = `url(${image})`
+      headshot.style.backgroundImage = `url(${image})`
     }, i * 100)
   })
   setTimeout(() => {
@@ -200,21 +200,22 @@ function toggleDarkMode() {
   backgroundColors = backgroundColors.reverse()
   textColors = textColors.reverse()
   
-  const body = document.querySelector('body')
-  const t0 = document.getElementById('text-wh-0')
-  const t1 = document.getElementById('text-wh-1')
-  const t2 = document.getElementById('text-wh-2')
-  const t3 = document.getElementById('text-wh-3')
-  const t4 = document.getElementById('contacts-wrapper')
-  const t5 = document.getElementById('footer')
-  const t6 = document.getElementById('down-arrow')
-  const wtArr = [t0, t1, t2, t3, t4, t5, t6]
+  const whiteBackgrounds = ['landing-name']
+  const blackBackgrounds = ['bd']
+  const whiteTextIds = ['landing-title','text-wh-0', 'text-wh-1', 'text-wh-2', 'text-wh-3', 'contacts-wrapper', 'footer', 'down-arrow', 'contactLink-0', 'contactLink-1', 'contactLink-2']
+  const blackTextIds = ['landing-name']
 
-  body.classList.add(backgroundColors[0])
-  body.classList.remove(backgroundColors[1])
+  blackBackgrounds.forEach(element => document.getElementById(element).classList.add(backgroundColors[0]))
+  blackBackgrounds.forEach(element => document.getElementById(element).classList.remove(backgroundColors[1]))
 
-  wtArr.forEach(element => element.classList.add(textColors[1]))
-  wtArr.forEach(element => element.classList.remove(textColors[0]))
+  whiteBackgrounds.forEach(element => document.getElementById(element).classList.add(backgroundColors[1]))
+  whiteBackgrounds.forEach(element => document.getElementById(element).classList.remove(backgroundColors[0]))
+
+  whiteTextIds.forEach(element => document.getElementById(element).classList.add(textColors[1]))
+  whiteTextIds.forEach(element => document.getElementById(element).classList.remove(textColors[0]))
+
+  blackTextIds.forEach(element => document.getElementById(element).classList.add(textColors[0]))
+  blackTextIds.forEach(element => document.getElementById(element).classList.remove(textColors[1]))
 }
 
 function displayIcons() {
@@ -245,11 +246,6 @@ function displayProjects() {
     element.classList.add('project-card')
     grid.appendChild(element)
 
-    // const title = document.createElement('div')
-    // title.innerHTML = project.name
-    // title.classList.add('project-title', 'red-background')
-    // grid.appendChild(title)
-
     const content = document.createElement('div')
     content.classList.add('project-content', 'background-black', 'text-white')
     element.appendChild(content)
@@ -264,14 +260,14 @@ function displayProjects() {
     content.appendChild(links)
 
     const githubLink = document.createElement('A')
-    githubLink.classList.add('github-link', 'devicon-github-plain')
+    githubLink.classList.add('github-link', 'text-white', 'devicon-github-plain')
     githubLink.href = project.urlGithub
     githubLink.target = '_blank'
     githubLink.rel = 'noreferrer'
     links.appendChild(githubLink)
 
     const liveLink = document.createElement('A')
-    liveLink.classList.add('live-link', (project.urlLive.slice(18, 24) === 'github' ? 'devicon-github-plain' : 'devicon-heroku-original'))
+    liveLink.classList.add('live-link', 'text-white', (project.urlLive.slice(18, 24) === 'github' ? 'devicon-github-plain' : 'devicon-heroku-original'))
     liveLink.href = project.urlLive
     liveLink.target = '_blank'
     liveLink.rel = 'noreferrer'
@@ -294,7 +290,8 @@ function displayContacts() {
   const container = document.getElementById('contacts-wrapper')
   contactsContent.forEach(contact => { 
     const element = document.createElement('a')
-    element.classList.add(contact.class.slice(0, 3), contact.class.slice(4), 'contact-link')
+    element.id = `contactLink-${contactsContent.indexOf(contact)}`
+    element.classList.add(contact.class.slice(0, 3), contact.class.slice(4), 'contact-link', 'text-white')
     element.href = contact.href
     element.target = '_blank'
     element.rel = 'noreferrer'
